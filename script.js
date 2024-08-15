2bb710ee856a457499d1b17fa34d74a84d070c81
ruben <174733720+ruben-23@users.noreply.github.com>
2024-08-15 17:32:28 +0300

    Add function that handles what math operation to execute

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function multiply(a, b){
 function divide(a, b) {
     return a/b;
 }
+
+function operate(firstNumber, operator, secondNumber) {
+
+    switch(operator) {
+        case '+':
+            alert(add(firstNumber, secondNumber));
+            break;
+        case '-':
+            alert(substract(firstNumber, secondNumber));
+            break;
+        case '*':
+            alert(multiply(firstNumber, secondNumber));
+            break;
+        case '/':
+            alert(divide(firstNumber, secondNumber));
+            break;    
+    }
+}
+
+const operationString = prompt("Enter the operation");
+
+let operationArray = operationString.split('');
+
+const firstNumber = parseInt(operationArray[0]);
+const operator = operationArray[1];
+const secondNumber = parseInt(operationArray[2]);
